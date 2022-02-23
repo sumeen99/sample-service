d4607940bea275b88d9aaf7b2e2153c4b22354fe
yoonjk <nexweb1@gmail.com>
2022-02-23 13:19:24 +0900

    Add Hello Project

---
+++ src/main/java/com/demo/microservices/model/Hello.java
@@ -0,0 +1,16 @@
+package com.demo.microservices.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class Hello {
+	private Long counter;
+	private String msg;
+}
+
